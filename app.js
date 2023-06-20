925954615a1579bc39e280fcb22107cf659f3bd4
dezea2007 <48436409+dezea2007@users.noreply.github.com>
2023-06-21 00:29:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -57,4 +57,4 @@ app.post("/failure", (req, res) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(procss.env.PORT || 3000, () => console.log("Server is running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"));
